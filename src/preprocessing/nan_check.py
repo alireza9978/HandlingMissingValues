966a807d737b2870690a672617c28c510246a896
alireza9978 <rahimialireza.az@gmail.com>
2021-09-28 14:31:49 +0330

    check files for nan existence with new method

--- src/preprocessing/nan_check.py
+++ src/preprocessing/nan_check.py
@@ -1,7 +1,26 @@
 import pandas as pd
+import os
 
-if __name__ == '__main__':
+
+def method_one():
     main_df = pd.read_csv("../../datasets/smart_star.csv")
     main_df.date = pd.to_datetime(main_df.date)
     nan_df = main_df.groupby("id").apply(lambda x: x.usage.isna().sum())
     nan_df.to_csv("../../results/nan.csv")
+
+
+def method_two():
+    root_path = "./../../datasets/apartment/"
+    years = ["2014", "2015", "2016"]
+    for year in years:
+        for item in os.listdir(root_path + year):
+            if item.endswith(".csv"):
+                item_path = root_path + year + "/" + item
+                temp_df = pd.read_csv(item_path, header=None)
+                temp_df.columns = ["date", "usage"]
+                if temp_df.usage.isna().sum() != 0:
+                    print(item_path)
+
+
+if __name__ == '__main__':
+    method_two()
