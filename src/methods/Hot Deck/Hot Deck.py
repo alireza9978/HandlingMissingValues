4e7929284c7b6ac0a2ecfb7fd29b914991129d98
alireza9978 <rahimialireza.az@gmail.com>
2021-10-09 23:19:15 +0330

    implementation of Hot Dec imputation added

--- src/methods/Hot Deck/Hot Deck.py
+++ src/methods/Hot Deck/Hot Deck.py
@@ -1,33 +1,33 @@
 import numpy as np
+import pandas as pd
 
-from src.measurements.Measurements import mean_square_error
+from src.measurements.Measurements import mean_square_error, evaluate_dataframe
 from src.preprocessing.load_dataset import get_dataset_with_modified_date
 
 
 def fill_nan(temp_array: np.ndarray):
-    print(temp_array)
 
-    # for idx, v in enumerate(missing):  # For each missing element in data set
-    #     i, j = v
-    #     euclidean = []
-    #     euclideanTotal = 0
-    #     for r in range(len(importedNM)):  # Loop all non-missing rows...
-    #         for c in range(col):  # ...and all of its columns...
-    #             if c != j:  # ...except itself...
-    #                 euclideanTotal += (imported[i][c] - importedNM[r][
-    #                     c]) ** 2  # ...to calculate the euclidean distance of both...
-    #         e = math.sqrt(euclideanTotal)
-    #         euclidean.append(
-    #             [e, importedNM_index[r]])  # Append found euclidean and index of that in the original data set
-    #     sorted(euclidean, key=lambda l: l[0], reverse=True)  # Sorts the euclidean list by their first value
-    #     lst = [imported[euclidean[r][1]][j] for r in range(kHD)]  # Gets the list of first kHD elements of those values
-    #     imported[i][j] = Counter(lst).most_common(1)[0][0]  # Imputes the most common element from above list.
-    #     printProgress(idx + 1, miss, v, imported[i][j])
+    nan_row = temp_array[temp_array["usage"].isna()]
+    temp_nan_index = nan_row.index.to_numpy()
+    complete_row = temp_array[~temp_array["usage"].isna()]
 
-    return None
+    def get_nearest_usage(row: pd.Series):
+        temp_row = row.drop(["id", "usage"]).to_numpy()
+
+        def distance(inner_row: pd.Series):
+            temp_usage = inner_row["usage"]
+            temp_distance = np.sum(np.square(inner_row.drop(["id", "usage"]).to_numpy() - temp_row))
+            return pd.Series([temp_distance, temp_usage])
+
+        calculated_distances = complete_row.apply(distance, axis=1)
+        calculated_distances = calculated_distances.sort_values(by=0)
+        return calculated_distances.loc[0][1]
+    filled_nan = nan_row.apply(get_nearest_usage, axis=1)
+    return pd.Series([filled_nan, temp_nan_index])
 
 
 if __name__ == '__main__':
     x, x_nan = get_dataset_with_modified_date()
-    filled_x, nan_index = x.groupby("id").apply(fill_nan(x_nan))
-    print(mean_square_error(x[nan_index], filled_x))
+    filled_users = x_nan.groupby("id").apply(fill_nan)
+    filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
+    print(evaluate_dataframe(filled_users, mean_square_error))
