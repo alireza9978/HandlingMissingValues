7f7add3c2e31f5bdbff1e7b7d09a0aee2bf42222
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-10-16 08:40:14 +0330

    add some preprocessing method for new dataset

--- src/methods/test.py
+++ src/methods/test.py
@@ -1,7 +1,8 @@
 import importlib
 
 from src.measurements.Measurements import *
-from src.preprocessing.load_dataset import get_dataset, get_dataset_with_modified_date
+from src.preprocessing.load_dataset import get_dataset
+from src.visualisation.visualization import plot_result
 
 measures = [mean_square_error, mean_absolute_error, mean_absolute_percentage_error]
 
@@ -23,18 +24,27 @@ methods_multiple_feature = [importlib.import_module("Regression.Linear"),
 method_name_multiple_feature = ["Linear Regression",
                                 "Hot Deck"]
 
+result_df = pd.DataFrame()
 x, x_nan = get_dataset()
 for i in range(len(method_name_single_feature)):
     filled_users = x_nan.groupby("id").apply(methods_single_feature[i].fill_nan)
     filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
     print(method_name_single_feature[i])
+    temp_result_list = [method_name_single_feature[i]]
     for measure in measures:
-        print(evaluate_dataframe(filled_users, measure))
+        measured_value = evaluate_dataframe(filled_users, measure)
+        temp_result_list.append(measured_value)
+        print(measured_value)
+    result_df = result_df.append(pd.Series(temp_result_list), ignore_index=True)
 
-x, x_nan = get_dataset_with_modified_date()
-for i in range(len(methods_multiple_feature)):
-    filled_users = x_nan.groupby("id").apply(methods_multiple_feature[i].fill_nan)
-    filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
-    print(method_name_multiple_feature[i])
-    for measure in measures:
-        print(evaluate_dataframe(filled_users, measure))
+# x, x_nan = get_dataset_with_modified_date()
+# for i in range(len(methods_multiple_feature)):
+#     filled_users = x_nan.groupby("id").apply(methods_multiple_feature[i].fill_nan)
+#     filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
+#     print(method_name_multiple_feature[i])
+#     for measure in measures:
+#         print(evaluate_dataframe(filled_users, measure))
+
+
+result_df.columns = ["name", "mean_square_error", "mean_absolute_error", "mean_absolute_percentage_error"]
+plot_result(result_df)
--- src/preprocessing/start_end_date.py
+++ src/preprocessing/start_end_date.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from src.preprocessing.load_dataset import load_weather_dataset
+from src.preprocessing.load_dataset import get_dataset_irish
 
 root = "/home/alireza/projects/python/HandlingMissingValues/"
 
@@ -17,11 +17,11 @@ def save_end_date(temp_df: pd.DataFrame, name: str):
     return end_date
 
 
-def make_smaller_dataset():
-    main_df = pd.read_csv(root + "datasets/smart_star_hourly.csv")
+def make_smaller_dataset(path, name):
+    main_df = pd.read_csv()
     main_df.date = pd.to_datetime(main_df.date)
-    start_date_df = save_start_date(main_df)
-    end_date_df = save_end_date(main_df)
+    start_date_df = save_start_date(main_df, "small_star_start")
+    end_date_df = save_end_date(main_df, "small_star_end")
     a = main_df.date > pd.to_datetime(start_date_df.max())
     b = main_df.date < pd.to_datetime(end_date_df.min())
     small_df = main_df[a & b]
@@ -29,7 +29,11 @@ def make_smaller_dataset():
 
 
 if __name__ == '__main__':
-    weather_df = load_weather_dataset()
-    weather_df["id"] = 1
-    save_end_date(weather_df, "weather_end")
-    save_start_date(weather_df, "weather_start")
+    # weather_df = load_weather_dataset()
+    # weather_df["id"] = 1
+    # save_end_date(weather_df, "weather_end")
+    # save_start_date(weather_df, "weather_start")
+    irish_df = get_dataset_irish()
+    start = save_end_date(irish_df, "irish_end")
+    end = save_start_date(irish_df, "irish_start")
+
