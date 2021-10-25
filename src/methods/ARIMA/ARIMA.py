dd0eea19819833b6bd3a31476a72c14467c1f0c9
Afshin <afshinshah77@gmail.com>
2021-10-25 13:03:38 +0330

    ARIMA - scaling added - Awaiting Test KNN - Awaiting Test

--- src/methods/ARIMA/ARIMA.py
+++ src/methods/ARIMA/ARIMA.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import MinMaxScaler
 from statsmodels.tsa.arima.model import ARIMA
 import pandas as pd
 import numpy as np
@@ -7,15 +8,22 @@ from src.preprocessing.load_dataset import get_dataset
 from src.utils.parallelizem import apply_parallel
 
 
+def normalize_user_usage(user):
+    scaler = MinMaxScaler()
+    user['usage'] = scaler.fit_transform(user['usage'].to_numpy().reshape(-1, 1))
+    return user, scaler
+
+
 def fill_nan(user_data):
     user = user_data.copy()
     # getting the nan indexes
     nan_row = user[user["usage"].isna()]
     nan_index = nan_row.index.to_numpy()
+    scaler, user = normalize_user_usage(user)
     model = ARIMA(user['usage'], order=(2, 1, 2))
     results = model.fit()
     user['usage'] = pd.Series(results.fittedvalues, copy=True)
-    predictions = user['usage'][nan_index]
+    predictions = scaler.inverse_transform(user['usage'][nan_index])
     # plt.plot(predictions)
     # plt.show()
     # plt.plot(user['usage'])
@@ -31,7 +39,5 @@ if __name__ == '__main__':
     # filled_users = fill_nan(x_nan)
     filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
     print(
-
-
 
         evaluate_dataframe(filled_users, mean_square_error))
