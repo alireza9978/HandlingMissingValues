3a281ca7d9e63ae7fab4230d7ad03957cd6d814a
alireza9978 <rahimialireza.az@gmail.com>
2021-11-20 14:57:30 +0330

    measure methods return error and loss

--- src/measurements/Measurements.py
+++ src/measurements/Measurements.py
@@ -7,21 +7,21 @@ def root_mean_square_error(x: np.ndarray, x_filled_nan: np.ndarray):
     diff2 = np.square(diff)
     diff2_mean = np.mean(diff2)
     root_diff2_mean = np.sqrt(diff2_mean)
-    return root_diff2_mean
+    return root_diff2_mean, diff2
 
 
 def mean_square_error(x: np.ndarray, x_filled_nan: np.ndarray):
     diff = x - x_filled_nan
     diff2 = np.square(diff)
     diff2_mean = np.mean(diff2)
-    return diff2_mean
+    return diff2_mean, diff2
 
 
 def mean_absolute_error(x: np.ndarray, x_filled_nan: np.ndarray):
     diff = x - x_filled_nan
     diff_abs = np.abs(diff)
     diff_abs_mean = np.mean(diff_abs)
-    return diff_abs_mean
+    return diff_abs_mean, diff_abs
 
 
 def mean_absolute_percentage_error(x: np.ndarray, x_filled_nan: np.ndarray):
@@ -32,7 +32,13 @@ def mean_absolute_percentage_error(x: np.ndarray, x_filled_nan: np.ndarray):
     diff_percent = np.divide(diff, x)
     diff_percent_abs = np.abs(diff_percent)
     diff_percent_abs_mean = np.mean(diff_percent_abs)
-    return np.multiply(diff_percent_abs_mean, 100)
+    return np.multiply(diff_percent_abs_mean, 100), diff_percent_abs
+
+
+def evaluate_dataframe_two(temp_df: pd.DataFrame, evaluation_function):
+    error, error_column = evaluation_function(temp_df["usage"], temp_df["predicted_usage"])
+    temp_df["error"] = error_column
+    return error, temp_df
 
 
 def evaluate_dataframe(temp_df: pd.DataFrame, evaluation_function):
