1ce8bf13f9e4b209a3f0c4a68b5c252494547d58
alireza9978 <rahimialireza.az@gmail.com>
2021-11-20 14:55:29 +0330

    change return type

--- src/utils/parallelizem.py
+++ src/utils/parallelizem.py
@@ -11,8 +11,8 @@ def apply_parallel(data_frame_grouped, func, args=None):
     if args is None:
         result_list = Parallel(n_jobs=int(multiprocessing.cpu_count()))(
             delayed(func)(group) for name, group in data_frame_grouped)
-        return pd.DataFrame(result_list)
+        return pd.concat(result_list)
     else:
         result_list = Parallel(n_jobs=int(multiprocessing.cpu_count()))(
             delayed(func)(group, args) for name, group in data_frame_grouped)
-        return pd.DataFrame(result_list)
+        return pd.concat(result_list)
