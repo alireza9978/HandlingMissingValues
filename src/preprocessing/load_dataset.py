bfaa5630c480e5d35d22b7b7e891eb2bb08e80e9
alireza9978 <rahimialireza.az@gmail.com>
2021-11-23 11:48:35 +0330

    add method to load proper dataset for DAE

--- src/preprocessing/load_dataset.py
+++ src/preprocessing/load_dataset.py
@@ -8,7 +8,18 @@ from src.preprocessing.convert_timestamps import convert_date
 # root = "E:/HandlingMissingValues/"
 # root = "/home/alireza/projects/python/HandlingMissingValues/"
 root = "/home/ippbx/PycharmProjects/HandlingMissingValues/"
-root = 'h:/Projects/Datasets/Smart/'
+# root = 'h:/Projects/Datasets/Smart/'
+
+
+def get_train_test_fully_modified_date(nan_percent: str, test_percent: float):
+    file_name = "smart_star_hourly_fully_modified"
+    train_df = pd.read_csv(Path(root + "datasets/train_test/{}_train_{}.csv".format(file_name, test_percent)))
+    test_df = pd.read_csv(Path(root + "datasets/train_test/{}_test_{}.csv".format(file_name, test_percent)))
+    train_nan_df = pd.read_csv(
+        Path(root + "datasets/train_test/with_nan/{}_train_{}_nan_{}.csv".format(file_name, test_percent, nan_percent)))
+    test_nan_df = pd.read_csv(
+        Path(root + "datasets/train_test/with_nan/{}_test_{}_nan_{}.csv".format(file_name, test_percent, nan_percent)))
+    return train_df, test_df, train_nan_df, test_nan_df
 
 
 def get_dataset(nan_percent: str):
@@ -41,6 +52,13 @@ def get_dataset_irish():
     main_df = pd.read_csv(Path("/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"))
     main_df.date = pd.to_datetime(main_df.date)
     return main_df
+
+
+def get_dataset_fully_modified_date_auto(nan_percent: str):
+    main_df_with_nan = pd.read_csv(Path(root + f"datasets/with_nan/smart_star_hourly_fully_modified_{nan_percent}.csv"))
+    main_df = pd.read_csv(Path(root + "datasets/smart_star_hourly_fully_modified.csv"))
+    main_df_with_nan["real_usage"] = main_df["usage"]
+    return main_df_with_nan
 
 
 def get_dataset_fully_modified_date(nan_percent: str):
