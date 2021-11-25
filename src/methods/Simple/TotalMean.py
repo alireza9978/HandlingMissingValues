2a1db286fba198f71ca220dbef1cede432f82ebc
alireza9978 <rahimialireza.az@gmail.com>
2021-11-25 16:49:10 +0330

    Imputation Methods updated - Architecture updated to incorporate object-oriented programming

--- src/methods/Simple/TotalMean.py
+++ src/methods/Simple/TotalMean.py
@@ -2,29 +2,71 @@ import numpy as np
 import pandas as pd
 
 from src.measurements.Measurements import evaluate_dataframe_two, mean_square_error
-from src.preprocessing.load_dataset import get_dataset
+from src.preprocessing.load_dataset import get_train_test_dataset
+from src.utils.parallelizem import apply_parallel, apply_parallel_two
 
 
-def fill_nan(temp_df: pd.DataFrame):
-    temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
-    final_filled_nan_index = temp_df.index[temp_df.usage.isna()].to_numpy()
-    temp_mean = np.nanmean(temp_array).sum()
-    filled_nan = np.nan_to_num(temp_array, nan=temp_mean)
-    temp_nan_index = np.where(np.isnan(temp_array))[0]
-    return pd.DataFrame({"predicted_usage": filled_nan[temp_nan_index].squeeze()},
-                        index=final_filled_nan_index.squeeze())
+class TotalMean:
+
+    def __init__(self, dfs):
+        train_df, test_df, train_nan_df, test_nan_df = dfs
+        self.train_df = train_df
+        self.test_df = test_df
+        self.train_nan_df = train_nan_df
+        self.test_nan_df = test_nan_df
+        self.params = None
+        self.train_result = None
+        self.train_error_df = None
+        self.test_result = None
+        self.test_error_df = None
+
+    def train(self):
+        output = apply_parallel_two(self.train_nan_df.groupby("id"), TotalMean.fill_nan)
+        self.train_result = pd.DataFrame()
+        self.params = {}
+        for row in output:
+            result_df = row[0]
+            self.train_result = self.train_result.append(result_df)
+            self.params[row[1]] = row[2]
+        self.train_result = self.train_result.join(self.train_df[["usage"]])
+        error, self.train_error_df = evaluate_dataframe_two(self.train_result, mean_square_error)
+        print("train error = ", error)
+
+    def test(self):
+        self.test_result = apply_parallel(self.test_nan_df.groupby("id"), TotalMean.fill_nan_test, self)
+        self.test_result = self.test_result.join(self.test_df[["usage"]])
+        error, self.test_error_df = evaluate_dataframe_two(self.test_result, mean_square_error)
+        print("test error = ", error)
+
+    @staticmethod
+    def fill_nan(temp_df):
+        user_id = temp_df["id"].values[0]
+        temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
+        final_filled_nan_index = temp_df.index[temp_df.usage.isna()].to_numpy()
+        temp_mean = np.nanmean(temp_array).sum()
+        filled_nan = np.nan_to_num(temp_array, nan=temp_mean)
+        temp_nan_index = np.where(np.isnan(temp_array))[0]
+        return pd.DataFrame({"predicted_usage": filled_nan[temp_nan_index].squeeze()},
+                            index=final_filled_nan_index.squeeze()), user_id, temp_mean
+
+    @staticmethod
+    def fill_nan_test(temp_df, self):
+        user_id = temp_df["id"].values[0]
+        param = self.params[user_id]
+        temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
+        final_filled_nan_index = temp_df.index[temp_df.usage.isna()].to_numpy()
+        filled_nan = np.nan_to_num(temp_array, nan=param)
+        temp_nan_index = np.where(np.isnan(temp_array))[0]
+        return pd.DataFrame({"predicted_usage": filled_nan[temp_nan_index].squeeze()},
+                            index=final_filled_nan_index.squeeze())
+
+    @staticmethod
+    def get_name():
+        return "total_mean"
 
 
 if __name__ == '__main__':
-    from src.utils.Methods import fill_nan as fn
-
     nan_percent = "0.01"
-    x, x_nan = get_dataset(nan_percent)
-    filled_users = fn(x, x_nan, fill_nan)
-    error, error_df = evaluate_dataframe_two(filled_users, mean_square_error)
-    print(error)
-    print(error_df)
-
-
-def get_name():
-    return "total_mean"
+    model = TotalMean(get_train_test_dataset(nan_percent, 0.3))
+    model.train()
+    model.test()
