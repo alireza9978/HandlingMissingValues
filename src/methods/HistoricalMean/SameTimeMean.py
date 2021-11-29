89fcc230f78d514360b8b7b70f0e50472b6aca16
alireza9978 <rahimialireza.az@gmail.com>
2021-11-29 08:54:44 +0330

    add method for missing value imputation which uses the mean of periodic times plot users

---
+++ src/methods/HistoricalMean/SameTimeMean.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+
+from src.measurements.Measurements import evaluate_dataframe, mean_square_error
+from src.preprocessing.load_dataset import get_dataset
+
+
+def fill_nan(temp_df: pd.DataFrame, window_size):
+    import swifter
+    _ = swifter.config
+    temp_nan_index = temp_df.usage.isna()
+    final_temp_nan_index = temp_df.index[temp_nan_index].to_numpy()
+    temp_df = temp_df.reset_index(drop=True)
+    temp_nan_index = temp_df.usage.isna()
+    temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
+    half_window_size = int(window_size / 2)
+    temp_array = np.pad(temp_array.squeeze(), half_window_size,
+                        mode="constant", constant_values=np.nan).reshape(-1, 1)
+
+    temp_df["hour"] = temp_df.date.dt.hour
+    temp_usage_mean = temp_df[["hour", "usage"]].groupby("hour").mean()
+
+    def inner_window_filler(nan_row):
+        # row_index = nan_row["row_index"] + half_window_size
+        # usage_window = np.concatenate([temp_array[row_index + 1: row_index + 1 + half_window_size],
+        #                                temp_array[row_index - half_window_size:row_index]])
+        return temp_usage_mean.loc[nan_row["hour"]].usage
+        # return np.nanmean(usage_window).sum()
+
+    temp_df["row_index"] = temp_df.index
+    filled_nan = temp_df[temp_nan_index].swifter.progress_bar(False). \
+        apply(inner_window_filler, axis=1).to_numpy().reshape(-1, 1)
+
+    return pd.Series([filled_nan, final_temp_nan_index])
+
+
+if __name__ == '__main__':
+    x, x_nan = get_dataset("0.01")
+    x = x[x.id == 55]
+    x_nan = x_nan[x_nan.id == 55]
+    filled_users = x_nan.groupby("id").apply(fill_nan, 4)
+    filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
+    print(evaluate_dataframe(filled_users, mean_square_error))
--- src/visualisation/UserPloting.py
+++ src/visualisation/UserPloting.py
@@ -3,13 +3,37 @@ import matplotlib.pyplot as plt
 from src.preprocessing.load_dataset import get_dataset, root
 import numpy as np
 
-x, x_nan = get_dataset()
-week_x = x.set_index("date")
-week_x = week_x.groupby("id").apply(lambda df: df.resample("1W").sum()[["usage"]]).reset_index()
-users_id = week_x.id.unique()
-sample_users = np.random.choice(users_id, 20)
-for user_id in sample_users:
-    temp_user = week_x[week_x.id == user_id]
-    plt.plot(temp_user.usage)
-    plt.savefig(root + f"results/user_figures/{user_id}.jpeg")
-    plt.close()
+
+def plot_random_users_usage(temp_df: pd.DataFrame):
+    week_x = temp_df.set_index("date")
+    week_x = week_x.groupby("id").apply(lambda df: df.resample("1W").sum()[["usage"]]).reset_index()
+    users_id = week_x.id.unique()
+    sample_users = np.random.choice(users_id, 20)
+    for user_id in sample_users:
+        temp_user = week_x[week_x.id == user_id]
+        plt.plot(temp_user.usage)
+        plt.savefig(root + f"results/user_figures/{user_id}.jpeg")
+        plt.close()
+
+
+def plot_users_mean_usage_in_day(temp_df: pd.DataFrame):
+    temp_df["hour"] = temp_df.date.dt.hour
+    mean_df = temp_df.groupby(["id", "hour"]).mean()
+    mean_df["std"] = temp_df.groupby(["id", "hour"]).std()
+    mean_df = mean_df.reset_index()
+    x_axis = np.arange(1, 25)
+
+    def inner_plot(inner_df: pd.DataFrame):
+        user_id = inner_df['id'].values[0]
+        name = "user = " + str(user_id)
+        plt.errorbar(x_axis, inner_df["usage"], inner_df["std"], marker='^', label=name)
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        plt.savefig(root + f"results/user_figures/{user_id}.jpeg")
+        plt.close()
+
+    mean_df.groupby(["id"]).apply(inner_plot)
+
+
+if __name__ == '__main__':
+    x, x_nan = get_dataset("0.01")
+    plot_users_mean_usage_in_day(x)
