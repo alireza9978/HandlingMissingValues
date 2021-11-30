eedb286a45dc45325f83baf012f15938de1a32c1
alireza9978 <rahimialireza.az@gmail.com>
2021-11-30 11:11:33 +0330

    update total median method

--- src/methods/Simple/TotalMedian.py
+++ src/methods/Simple/TotalMedian.py
@@ -1,58 +1,33 @@
 import numpy as np
 import pandas as pd
 
-from src.measurements.Measurements import mean_square_error, evaluate_dataframe_two
+from src.methods.BaseModel.Base import Base
 from src.preprocessing.load_dataset import get_train_test_dataset
-from src.utils.parallelizem import apply_parallel, apply_parallel_two
 
 
-class TotalMedian:
+class TotalMedian(Base):
 
-    def __init__(self, dfs):
-        train_df, test_df, train_nan_df, test_nan_df = dfs
-        self.train_df = train_df
-        self.test_df = test_df
-        self.train_nan_df = train_nan_df
-        self.test_nan_df = test_nan_df
-        self.params = None
-        self.train_result = None
-        self.train_error_df = None
-        self.test_result = None
-        self.test_error_df = None
-
-    def train(self):
-        output = apply_parallel_two(self.train_nan_df.groupby("id"), TotalMedian.fill_nan)
-        self.train_result = pd.DataFrame()
-        self.params = {}
-        for row in output:
-            result_df = row[0]
-            self.train_result = self.train_result.append(result_df)
-            self.params[row[1]] = row[2]
-        self.train_result = self.train_result.join(self.train_df[["usage"]])
-        error, self.train_error_df = evaluate_dataframe_two(self.train_result, mean_square_error)
-        print("train error = ", error)
-
-    def test(self):
-        self.test_result = apply_parallel(self.test_nan_df.groupby("id"), TotalMedian.fill_nan_test, self)
-        self.test_result = self.test_result.join(self.test_df[["usage"]])
-        error, self.test_error_df = evaluate_dataframe_two(self.test_result, mean_square_error)
-        print("test error = ", error)
+    def train_test_save(self, nan_percent_value):
+        super().train(TotalMedian.get_train_params(), TotalMedian.fill_nan)
+        super().test(TotalMedian.get_train_params(), TotalMedian.fill_nan_test)
+        super().save_result(TotalMedian.get_name(), nan_percent_value)
 
     @staticmethod
-    def fill_nan(temp_df: pd.DataFrame):
+    def fill_nan(temp_df: pd.DataFrame, _):
         user_id = temp_df["id"].values[0]
         temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
         final_filled_nan_index = temp_df.index[temp_df.usage.isna()].to_numpy()
-        temp_mean = np.nanmedian(temp_array).sum()
-        filled_nan = np.nan_to_num(temp_array, nan=temp_mean)
+        temp_median = np.nanmedian(temp_array).sum()
+        filled_nan = np.nan_to_num(temp_array, nan=temp_median)
         temp_nan_index = np.where(np.isnan(temp_array))[0]
         return pd.DataFrame({"predicted_usage": filled_nan[temp_nan_index].squeeze()},
-                            index=final_filled_nan_index.squeeze()), user_id, temp_mean
+                            index=final_filled_nan_index.squeeze()), user_id, temp_median
 
     @staticmethod
-    def fill_nan_test(temp_df, self):
+    def fill_nan_test(temp_df, other_input):
+        self, train_param = other_input
         user_id = temp_df["id"].values[0]
-        param = self.params[user_id]
+        param = self.params[str(train_param)][user_id]
         temp_array = temp_df.usage.to_numpy().reshape(-1, 1)
         final_filled_nan_index = temp_df.index[temp_df.usage.isna()].to_numpy()
         filled_nan = np.nan_to_num(temp_array, nan=param)
@@ -64,9 +39,12 @@ class TotalMedian:
     def get_name():
         return "total_median"
 
+    @staticmethod
+    def get_train_params():
+        return ["none"]
+
 
 if __name__ == '__main__':
     nan_percent = "0.01"
     model = TotalMedian(get_train_test_dataset(nan_percent, 0.3))
-    model.train()
-    model.test()
+    model.train_test_save(nan_percent)
