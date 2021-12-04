b8a2da832ab588e639280a079cb0bb15820ffda0
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-12-04 11:42:01 +0330

    update KMeans - update architecture - support train and test

--- src/methods/Clustring/KMeans.py
+++ src/methods/Clustring/KMeans.py
@@ -1,36 +1,67 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 
-from src.measurements.Measurements import evaluate_dataframe, mean_square_error
-from src.preprocessing.load_dataset import get_dataset_fully_modified_date
-from src.utils.parallelizem import apply_parallel
+from src.methods.BaseModel.Base import Base
+from src.preprocessing.load_dataset import get_train_test_fully_modified_date
 
 
-def fill_nan(temp_df: pd.DataFrame, n_clusters):
-    nan_row = temp_df[temp_df["usage"].isna()]
-    temp_nan_index = nan_row.index.to_numpy()
-    complete_row = temp_df[~temp_df["usage"].isna()]
+class Kmeans(Base):
 
-    x_train = complete_row.drop(columns=["id", "usage"]).to_numpy()
-    x_test = nan_row.drop(columns=["id", "usage"]).to_numpy()
-    # clustering all complete rows
-    clu = KMeans(n_clusters=n_clusters)
-    clu = clu.fit(x_train)
-    # predicting each row's cluster based on not nan rows with trained model
-    y_test = clu.predict(x_test)
-    y_test = pd.DataFrame(y_test, columns=["cluster_label"])
+    def train_test_save(self, nan_percent_value):
+        super().train(Kmeans.get_train_params(), Kmeans.fill_nan)
+        super().test(Kmeans.get_train_params(), Kmeans.fill_nan_test)
+        super().save_result(Kmeans.get_name(), nan_percent_value)
 
-    # impute missing values with mean value of each cluster
-    complete_row.insert(0, "cluster_label", clu.labels_, True)
-    clusters_mean = complete_row.groupby("cluster_label").agg({"usage": "mean"})
-    filled_nan = y_test.set_index("cluster_label").join(clusters_mean).reset_index()["usage"]
+    @staticmethod
+    def fill_nan(temp_df: pd.DataFrame, n_clusters):
+        user_id = temp_df["id"].values[0]
+        temp_df = temp_df.drop(columns=["id"])
 
-    return pd.Series([filled_nan.to_numpy().reshape(-1, 1), temp_nan_index])
+        nan_row = temp_df[temp_df["usage"].isna()].drop(columns=["usage"])
+        temp_nan_index = nan_row.index.to_numpy()
+        complete_row = temp_df[~temp_df["usage"].isna()]
+
+        x_train = complete_row.drop(columns=["usage"]).to_numpy()
+        # clustering all complete rows
+        clu = KMeans(n_clusters=n_clusters)
+        clu = clu.fit(x_train)
+
+        # impute missing values with mean value of each cluster
+        complete_row.insert(0, "cluster_label", clu.labels_, True)
+        clusters_mean = complete_row.groupby("cluster_label").agg({"usage": "mean"})
+
+        y_pred = clu.predict(nan_row.to_numpy())
+        nan_row["cluster_label"] = pd.Series(y_pred, index=nan_row.index)
+        filled_nan = nan_row.set_index("cluster_label").join(clusters_mean).reset_index()["usage"]
+
+        return pd.DataFrame({"predicted_usage": filled_nan.to_numpy().squeeze()},
+                            index=temp_nan_index.squeeze()), user_id, (clu, clusters_mean)
+
+    @staticmethod
+    def fill_nan_test(temp_df: pd.DataFrame, other_input):
+        self, train_param = other_input
+        user_id = temp_df["id"].values[0]
+        temp_df = temp_df.drop(columns=["id"])
+        clu, clusters_mean = self.params[str(train_param)][user_id]
+
+        nan_row = temp_df[temp_df["usage"].isna()].drop(columns=["usage"])
+        nan_index = nan_row.index.to_numpy()
+        y_pred = clu.predict(nan_row.to_numpy())
+        nan_row["cluster_label"] = pd.Series(y_pred, index=nan_row.index)
+        filled_nan = nan_row.set_index("cluster_label").join(clusters_mean).reset_index()["usage"]
+
+        return pd.DataFrame({"predicted_usage": filled_nan.to_numpy().squeeze()}, index=nan_index.squeeze())
+
+    @staticmethod
+    def get_train_params():
+        return [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    @staticmethod
+    def get_name():
+        return "kmeans"
 
 
 if __name__ == '__main__':
-    x, x_nan = get_dataset_fully_modified_date("0.01")
-    for i in range(2, 10):
-        filled_users = apply_parallel(x_nan.groupby("id"), fill_nan, i)
-        filled_users[2] = filled_users[1].apply(lambda idx: x.loc[idx])
-        print(i, evaluate_dataframe(filled_users, mean_square_error))
+    nan_percent = "0.01"
+    model = Kmeans(get_train_test_fully_modified_date(nan_percent, 0.3))
+    model.train_test_save(nan_percent)
